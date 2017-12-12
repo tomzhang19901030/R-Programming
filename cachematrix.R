c69545c14b8e9ac2cdc8215bdab5a45b9d63d95c
tomzhang19901030 <34419002+tomzhang19901030@users.noreply.github.com>
2017-12-12 19:11:35 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,10 @@
+## Caching the Inverse of a Matrix:
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
 
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCaheMatrix = function(x = matrix()){
         
@@ -16,8 +22,36 @@ makeCaheMatrix = function(x = matrix()){
              getInverse = getInverse)
 }
 
+## Caching the Inverse of a Matrix:
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
+
+## This function creates a special "matrix" object that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL                          ## Initialize Null Matrix
+        set <- function(y) {                 ## Define the set function to 
+                x <<- y                      ## Assign now value of matrix 
+                inv <<- NULL                 ## if there is a new matrix, reset inv to null
+        }
+        get <- function() x                  ## define get function 
+        setInverse <- function(inverse) inv <<- inverse  ## assigns value of inverse in parent environment
+        getInverse <- function() inv                     ## gets the value of inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
+
+
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
+
 cacheSolve = function(x,...){
-        
+        ## return a matrix that is the inverse of X
         invmat = x$getInverse()
         if (!is.null(invmat)){
                 message("getting cached data")
